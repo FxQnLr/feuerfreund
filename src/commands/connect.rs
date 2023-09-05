297dc7af9e37a8416f8a2cd108298b2c55ec5038
fx <felixquinn03@gmail.com>
2023-09-05 21:35:45 +0200

    implemented random looping and lessened intents

--- src/commands/connect.rs
+++ src/commands/connect.rs
@@ -1,9 +1,38 @@
 use std::fs;
 
 use rand::{Rng, rngs::StdRng, SeedableRng};
-use songbird::input::Input;
+use serenity::async_trait;
+use songbird::{Event, TrackEvent, id::GuildId, EventHandler, EventContext, input::Input};
+use tracing::{info, error};
 
 use crate::{Error, Context};
+
+struct TrackEndNotifier {
+    guild_id: GuildId,
+    context: serenity::prelude::Context,
+}
+
+#[async_trait]
+impl EventHandler for TrackEndNotifier {
+    async fn act(&self, ctx: &EventContext<'_>) -> Option<Event> {
+        if let EventContext::Track(_) = ctx {
+            info!("track ended, playing new one");
+            let manager = songbird::get(&self.context).await
+                .expect("Songbird Voice client placed in at initialisation.")
+                .clone();
+
+            let handler = manager.get(self.guild_id);
+            if handler.is_none() {
+                error!("Error no handler")
+            }
+
+            handler.unwrap().lock().await.play_source(get_source().await.unwrap());
+            
+        }
+
+        None
+    }
+}
 
 #[poise::command(
     slash_command,
@@ -29,6 +58,28 @@ pub async fn connect(
 
     let handler = manager.join(guild.id, vc).await.0;
     
+
+    let mut handle = handler.lock().await;
+
+    handle.deafen(true).await?;
+
+    handle.play_source(get_source().await?);
+
+    handle.add_global_event(
+        Event::Track(TrackEvent::End),
+        TrackEndNotifier {
+            guild_id: guild.id.into(),
+            context: ctx.serenity_context().to_owned(),
+        },
+    );
+
+    ctx.say("connected").await?;
+
+    Ok(())
+}
+
+
+async fn get_source() -> Result<Input, songbird::input::error::Error> {
     let mut tracks = vec![];
     for entry in fs::read_dir("./media/")? {
         let entry = entry?;
@@ -48,10 +99,5 @@ pub async fn connect(
 
     println!("{}", track);
 
-    let source = songbird::ffmpeg(format!("./media/{}", track)).await?;
-    handler.lock().await.play_source(source);
-
-    ctx.say("connecting").await?;
-
-    Ok(())
+    songbird::ffmpeg(format!("./media/{}", track)).await
 }
