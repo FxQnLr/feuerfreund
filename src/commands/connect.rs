4a362eadf8f64c1ed0c8fa628a04968794c4bcf4
fx <felixquinn03@gmail.com>
2023-09-05 13:52:11 +0200

    multiple mp3 support, random selection

--- src/commands/connect.rs
+++ src/commands/connect.rs
@@ -1,3 +1,8 @@
+use std::fs;
+
+use rand::{Rng, rngs::StdRng, SeedableRng};
+use songbird::input::Input;
+
 use crate::{Error, Context};
 
 #[poise::command(
@@ -12,8 +17,6 @@ pub async fn connect(
     // TODO: Error handling
     let guild = ctx.guild().unwrap();
     
-    // let member = guild.member(ctx, ctx.author().id).await?;
-
     // TODO: Error handling
     let states = guild.voice_states.get(&ctx.author().id).unwrap();
 
@@ -24,11 +27,29 @@ pub async fn connect(
         .expect("Songbird Voice client placed in at initialisation.")
         .clone();
 
-    let _handler = manager.join(guild.id, vc).await.0;
+    let handler = manager.join(guild.id, vc).await.0;
+    
+    let mut tracks = vec![];
+    for entry in fs::read_dir("./media/")? {
+        let entry = entry?;
+        let name = entry.file_name().to_string_lossy().to_string();
+        let split = name.split('.').collect::<Vec<&str>>();
+        let end = split[split.len() - 1];
+        if end != "mp3" { continue; }
+        tracks.push(name);
+    };
 
-    let source = songbird::ffmpeg("./media/fire.mp3").await?;
+    println!("{:?}", tracks);
 
-    _handler.lock().await.play_source(source);
+    let mut rng = StdRng::from_entropy();
+    let t_id: usize = rng.gen_range(0..tracks.len());
+    println!("{}", t_id);
+    let track = &tracks[t_id];
+
+    println!("{}", track);
+
+    let source = songbird::ffmpeg(format!("./media/{}", track)).await?;
+    handler.lock().await.play_source(source);
 
     ctx.say("connecting").await?;
 
